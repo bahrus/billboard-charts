bfcdc09bb55d744b19e41353b5cf8125f9ed0860
bahrus <anderson.bruce.b@gmail.com>
2017-09-10 07:46:10 -0400

    billboard charts

--- billboard-charts.js
+++ billboard-charts.js
@@ -21,12 +21,39 @@ var xtal;
                 get chart() {
                     return this._chart;
                 }
+                //from https://stackoverflow.com/questions/14780350/convert-relative-path-to-absolute-using-javascript
+                absolute(base, relative) {
+                    var stack = base.split("/"), parts = relative.split("/");
+                    stack.pop(); // remove current file name (or empty string)
+                    // (omit if "base" is the current folder without trailing slash)
+                    for (var i = 0; i < parts.length; i++) {
+                        if (parts[i] == ".")
+                            continue;
+                        if (parts[i] == "..")
+                            stack.pop();
+                        else
+                            stack.push(parts[i]);
+                    }
+                    return stack.join("/");
+                }
+                connectedCallback() {
+                    super.connectedCallback();
+                    if (!this.cssPath) {
+                        const cs = document.currentScript;
+                        if (cs) {
+                            this.cssPath = this.absolute(cs.baseURI, '../billboard.js/dist/billboard.css');
+                        }
+                        else {
+                            this.cssPath = '/bower_components/billboard.js/dist/billboard.css';
+                        }
+                    }
+                }
                 static get is() { return 'billboard-charts'; }
                 static get properties() {
+                    console.log({ currentScript: document.currentScript });
                     return {
                         cssPath: {
-                            type: String,
-                            value: '../../billboard.js/dist/billboard.css'
+                            type: String
                         },
                         publish: {
                             type: Boolean,
@@ -44,7 +71,7 @@ var xtal;
                             type: Object,
                             readOnly: true,
                             notify: true,
-                        }
+                        },
                     };
                 }
                 loaded() {
